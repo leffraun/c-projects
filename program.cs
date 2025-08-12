1a9ed8b944fb8a2629bf857763f971ce28f96a50
leffraun <143820813+leffraun@users.noreply.github.com>
2025-08-12 21:02:20 +0530

    number guessing game
    features 
    - manually inputed number of guesses
    - play again features
    - hint system
    - input vaildation

--- program.cs
+++ program.cs
@@ -1,4 +1,4 @@
-
+//number guessing game
 // See https://aka.ms/new-console-template for more information
 using System;
 using System.Security.Cryptography.X509Certificates;
@@ -94,4 +94,5 @@ namespace MyApp
         }
     }
 }
+
 
